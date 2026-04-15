d9bb5aebb956226ab6160c09ecacf7fd433b80b3
smoczyna <jsmorczewski@gmail.com>
2026-04-15 14:27:48 +0100

    Update init_schema.py
    private data and credentials removed

--- test/create_db/init_schema.py
+++ test/create_db/init_schema.py
@@ -14,10 +14,10 @@ class InitDbTestCase(unittest.TestCase):
         # add master user if does not exist
         if len(data) == 0:
             Database.insert("users",
-                            {"_id": "404a4558725e4e1fb12dca5094c69729", "username": "smok", "user_type": "admin",
-                             "role": "ROLE_ADMIN", "first_name": "Jaroslaw", "last_name": "Smorczewski",
-                             "email": "jsmorczewski@gmail.com", "active": True, "deleted": False,
-                             "password": "$pbkdf2-sha512$25000$QiiF0BojBMDY.3/vvRdibA$0GoqRw3QggvfVwwFLGkfxMOEFQJKxHV.qB95m4PM69AxOpKJdI3yxHg7mokXkOB2qt.nPohPMypVejDs/b7a0A"})
+                            {"_id": "", "username": "", "user_type": "admin",
+                             "role": "ROLE_ADMIN", "first_name": "", "last_name": "",
+                             "email": "", "active": True, "deleted": False,
+                             "password": ""})
 
     def test_add_roles(self):
         data = Database.find("roles", {})
